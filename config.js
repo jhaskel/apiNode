bb55edcad3e57273b7f3a9d9019a864adff7c52b
JoHaskel <2bitsw@gmail.com>
2020-05-15 14:26:59 -0300

    second commit

---
+++ config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb+srv://Haskel:Haskel00*@apinode-wiz7n.azure.mongodb.net/test',
+    sendgridKey: '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',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=apinode1;AccountKey=EpyCiuP2Y5MKu7R6ablE0fJ7r0H7hIsCJodH+HTPJokJZzWqQ5Ddir0Y/nVHNfACOKklUmnTdLGfi20HFFnU8Q==;EndpointSuffix=core.windows.net'
+}
